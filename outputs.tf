4cc99da4131a1ed54b8cb85c2864e5084aca388d
fanor.quintero <fanor.quintero@davivienda.com>
2025-04-29 14:41:34 -0500

    test

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = aws_vpc.vpc[0].id
+  value       = aws_vpc.vpc.*.id
 }
 
 output "private_subnet_ids" {
   description = "The IDs of the private subnets"
-  value       = aws_subnet.private_subnet[*].id
+  value       = aws_subnet.private_subnet.*.id
 }
 
 output "database_subnet_ids" {
   description = "The IDs of the database subnets"
-  value       = aws_subnet.database_subnet[*].id
+  value       = aws_subnet.database_subnet.*.id
 }
