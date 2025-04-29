fdc87b1dc69ab5c18dac630cbde30cee2d27e79b
fanor.quintero <fanor.quintero@davivienda.com>
2025-04-29 14:24:24 -0500

    test

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_subnet" "database_subnet" {
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "database-subnet-${var.name}-${[for az in var.availability_zones : split("-", az)[2]][count.index]}"
+    Name = "database-subnet-${var.name}-${[for az in var.availability_zones : split("-", az)[2]]}"
   }
 
 }
